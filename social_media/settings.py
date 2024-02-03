415c99ad3ce116544262c0a9789d30d796df2496
shyam <shyam.bista3210@gmail.com>
2024-02-03 20:25:29 +0545

    bug fixed

--- social_media/settings.py
+++ social_media/settings.py
@@ -78,25 +78,25 @@ WSGI_APPLICATION = 'social_media.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / "db.sqlite3",
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
+    }
+}
 
 
 # Database connection -> postgresql
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT')
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.environ.get('DB_NAME'),
+#         'USER': os.environ.get('DB_USER'),
+#         'PASSWORD': os.environ.get('DB_PASSWORD'),
+#         'HOST': os.environ.get('DB_HOST'),
+#         'PORT': os.environ.get('DB_PORT')
+#     }
+# }
 
 
 # Password validation
@@ -152,8 +152,6 @@ STATIC_ROOT = BASE_DIR/'assets'
 MEDIA_ROOT = BASE_DIR/'media'
 
 MEDIA_URL = '/media/'
-
-
 
 
 AUTH_USER_MODEL = 'auth_app.CustomUser'
